bfdfc5c5abc8ee093cb3f9ccdff452b99de21414
anilbothe <anil_bothe@yahoo.com>
2022-05-10 18:38:14 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    loader: "akamai",
+    path: ""
+  },
+  basePath: "",
+  assetPrefix: ""
 }
 
 module.exports = nextConfig
